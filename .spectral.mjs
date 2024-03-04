d23e4b0cc98222db6eea8c17d9de7d466a3ba77e
Yannick Schuchmann <y.schuchmann@extern.deutsche-glasfaser.de>
2024-03-04 11:15:58 +0100

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzODc4OA/spectral.js?branch_slug=main&token=a83f9e46-49f1-42d5-8e76-e769fe487025";
+export default { extends: ruleset };
